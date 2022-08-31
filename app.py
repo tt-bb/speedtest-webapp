bca3a16f7f45d3aec49008e6b7355549b7e7f772
tt-bb <ttbb_@outlook.com>
2022-08-31 03:36:37 +0200

    Get IP from visitor and not server

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
         print("in progress...")
 
         # Get IP information
-        ip = request.remote_addr
+        ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
         print(ip)
         key = os.environ['KEY']
         url = f"https://ipgeolocation.abstractapi.com/v1/?api_key={key}&ip_address={ip}"
